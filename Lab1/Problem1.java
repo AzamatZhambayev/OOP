59252c7cbfc15437f4fa6e01850cdf8499333de4
Zhambo <azamat.zhambaev30@gmail.com>
2026-01-27 22:54:08 +0500

    first commit

---
+++ Lab1/Problem1.java
@@ -0,0 +1,23 @@
+public class PrintMyName {
+    public static void main(String[] args) {
+        String name = "Zhambik";   // change to your name
+        int len = name.length();
+
+        // top border
+        System.out.print("+");
+        for (int i = 0; i < len + 2; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+
+        // middle
+        System.out.println("| " + name + " |");
+
+        // bottom border
+        System.out.print("+");
+        for (int i = 0; i < len + 2; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
